51848f833588dc428f3d2397b838cea87a90477b
TCVD <viktordakov97@gmail.com>
2014-10-14 23:38:54 +0300

    Added admin panel for products and db changes.

---
+++ Sisbg.Models/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisbg.Models
+{
+    public class File
+    {
+        public int Id { get; set; }
+
+        public string Path { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
---
+++ Sisbg.Models/Length.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisbg.Models
+{
+    public class Length
+    {
+        public int Id { get; set; }
+
+        public int Inches { get; set; }
+
+        public string Code { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Pallet { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
